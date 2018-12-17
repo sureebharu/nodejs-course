f8869178788102aa5c59505592fb4480daf05a27
Bharathi S <bharathi.selvarajan@gmail.com>
2018-12-17 11:14:31 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fs= require("fs");
+const hbs = require("hbs");
+const app = express()
+const port = 3000;
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+app.use((req,res,next)=>{
+ var now = new Date().toString();
+ var log = `${now}:${req.method}:${req.url}`;
+ fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err){
+    console.log("Unable to process");
+    }
+ });
+ console.log(log);
+ next();
+})
+app.use((req,res)=>{
+    res.render('maintenance.hbs');
+})
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        title:'Welcome',
+        year:new Date().getFullYear(),
+        head:'Welcome to my page'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        title:'help',
+        head:'help page',
+        year:new Date().getFullYear()
+    });
+})
+app.listen(port,()=>console.log(`Example app listening on port ${port}`));
