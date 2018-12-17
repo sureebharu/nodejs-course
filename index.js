eb80fdc6e4058630f1553895ba3ebd21980415fc
Bharathi S <bharathi.selvarajan@gmail.com>
2018-12-17 12:48:08 +0530

    Set up for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs= require("fs");
 const hbs = require("hbs");
 const app = express()
-const port = 3000;
+const port = process.env.PORT||3000;
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 app.use(express.static(__dirname+'/public'));
